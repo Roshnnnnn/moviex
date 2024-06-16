9040787999bee70ec93f74853774b26a65348673
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-06-16 23:32:32 +0530

    update hero banner

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./style.scss";
 import { useNavigate } from "react-router-dom";
 import useFetch from "../../../hooks/useFetch";
